12f3102a8f3bb74602e59d78350d78fe86ec7027
helenabagels <hnico933@gmail.com>
2025-07-03 20:03:21 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,6 +2,7 @@ function validateLogin() {
   const user = document.getElementById("username").value;
   const pass = document.getElementById("password").value;
   if (user === "jabespogi" && pass === "amazing") {
+    localStorage.setItem("loggedInUser", "Jabez Mendoza");
     window.location.href = "index.html";
     return false;
   } else {
